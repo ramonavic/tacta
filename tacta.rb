8f5e8b2a8f9be2d609f7e83a59d822649548df04
ramonavic <ramon.hofmeister@gmail.com>
2016-06-09 11:22:18 +0200

    Added action checks and error message

--- tacta.rb
+++ tacta.rb
@@ -1,3 +1,5 @@
+# tacta.rb
+
 def index(contacts)
   contacts.each_with_index do |contact, i|
      puts "#{i+1}) #{contact[:name]}"
@@ -22,6 +24,26 @@ def action_show( contacts, i )
 
    puts
    show( contact )
+   puts
+end
+
+def action_delete( contacts )
+   puts
+   response = ask "Delete which contact? "
+
+   i = response.to_i
+
+   puts
+   puts "Contact for #{contacts[i-1][:name]} deleted."
+
+   contacts.delete_at( i-1 )
+
+   puts
+end
+
+def action_error
+   puts
+   puts "Sorry, I don't recognize that command."
    puts
 end
 
@@ -50,18 +72,10 @@ def ask(prompt)
    gets.chomp
 end
 
-def action_delete( contacts )
-   puts
-   response = ask "Delete which contact? "
-
-   i = response.to_i
-
-   puts
-   puts "Contact for #{contacts[i-1][:name]} deleted."
-
-   contacts.delete_at( i-1 )
-
-   puts
+def contact_exists?(contacts, response)
+  return false unless response =~ /[0-9]+/
+  i = response.to_i
+  !contacts[i-1].nil?
 end
 
 contacts = []
@@ -73,18 +87,26 @@ contacts << { name: "Genghis Khan"    , phone: "+976 2 194 2222" , email: "conta
 contacts << { name: "Malcom X"        , phone: "+1 310 155 8822" , email: "x@theroost.org"     }
 
 loop do
-   index( contacts )
+  index( contacts )
 
-   puts
-   response = ask "Who would you like to see (n for new, d for delete, q to quit)? "
+  puts
+  response = ask "Who would you like to see (n for new, d for delete, q to quit)? "
 
-   break if response == "q"
+  break if response == "q"
 
-   if response == "n"
-      action_new( contacts )
-   elsif response == "d"
-      action_delete( contacts )
-   else
+  if response == "n"
+    action_new( contacts )
+  elsif response == "d"
+    action_delete( contacts )
+  elsif response =~ /[0-9]+/
+    if contact_exists?(contacts, response)
       action_show( contacts, response.to_i )
-   end
+    else
+      puts
+      puts "That contact does not exist!"
+      puts
+    end
+  else
+    action_error
+  end
 end
